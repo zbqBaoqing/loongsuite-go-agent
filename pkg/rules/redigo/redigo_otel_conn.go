113a20981ea1b6b7df29cae59c865d8c92b6b487
Jose Arellanes <jose.arellanesaldaco@students.dominican.edu>
2025-10-08 19:52:37 -0700

    feat: include transaction queue to display all items (#592)
    * feat: include transaction queue to display all items
    
    * chore: updating redigo transaction test to include db statement change
    
    * feat: updating redigo otel conn to remove extra spacing
    
    * feat: purge transaction queue after use

--- pkg/rules/redigo/redigo_otel_conn.go
+++ pkg/rules/redigo/redigo_otel_conn.go
@@ -17,9 +17,11 @@ package redigo
 import (
 	"container/list"
 	"context"
+	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -28,7 +30,7 @@ const max_queue_length = 2048
 var configuredQueueLength int
 
 var commandQueue = list.New()
-
+var transactionQueue = list.New() // transaction queue
 var redigoInstrumenter = BuildRedigoInstrumenter()
 
 type armsConn struct {
@@ -58,7 +60,41 @@ func (a *armsConn) Do(commandName string, args ...interface{}) (reply interface{
 	startTime := time.Now()
 	reply, err = a.Conn.Do(commandName, args...)
 	endTime := time.Now()
-	redigoInstrumenter.StartAndEnd(ctx, req, nil, err, startTime, endTime)
+	switch strings.ToLower(commandName) {
+	case "multi":
+		// Start of a transaction
+		push(req, transactionQueue)
+	case "exec":
+		// End of a transaction, we need to build the command string for all commands in the transaction
+		string_builder := strings.Builder{}
+		for transactionQueue.Len() > 0 {
+			r := pop(transactionQueue)
+			if r != nil && strings.ToLower(r.cmd) != "multi" {
+				string_builder.WriteString(r.cmd + " ")
+				for i, arg := range r.args {
+					string_builder.WriteString(fmt.Sprintf("%v", arg))
+					if i < len(r.args)-1 {
+						string_builder.WriteString(" ")
+					}
+				}
+				string_builder.WriteString("; ")
+			}
+		}
+		req.cmd = "EXEC"
+		req.args = []interface{}{string_builder.String()}
+		redigoInstrumenter.StartAndEnd(ctx, req, nil, err, startTime, endTime)
+		transactionQueue.Init() // purge the transaction queue after EXEC
+	case "discard":
+		// Purge the transaction queue
+		transactionQueue.Init()
+	default:
+		// Inspect if we are in a transaction, otherwise just push to the command queue
+		if transactionQueue.Len() > 0 {
+			push(req, transactionQueue)
+			return
+		}
+		redigoInstrumenter.StartAndEnd(ctx, req, nil, err, startTime, endTime)
+	}
 	return
 }
 
@@ -75,7 +111,21 @@ func (a *armsConn) Send(commandName string, args ...interface{}) error {
 		ctx = context.Background()
 	}
 	req.ctx = ctx
-	push(req)
+	switch strings.ToLower(commandName) {
+	case "multi":
+		// Start of a transaction
+		push(req, transactionQueue)
+	case "discard":
+		// Purge the transaction queue
+		transactionQueue.Init()
+	default:
+		// Inspect if we are in a transaction, otherwise just push to the command queue
+		if transactionQueue.Len() > 0 {
+			push(req, transactionQueue)
+			return a.Conn.Send(commandName, args...)
+		}
+		push(req, commandQueue)
+	}
 	return a.Conn.Send(commandName, args...)
 }
 
@@ -85,7 +135,7 @@ func (a *armsConn) Flush() error {
 
 func (a *armsConn) Receive() (reply interface{}, err error) {
 	reply, err = a.Conn.Receive()
-	req := pop()
+	req := pop(commandQueue)
 	if req != nil {
 		now := time.Now()
 		redigoInstrumenter.StartAndEnd(req.ctx, req, nil, err, req.startTime, now)
@@ -93,16 +143,16 @@ func (a *armsConn) Receive() (reply interface{}, err error) {
 	return
 }
 
-func push(request *redigoRequest) {
-	if commandQueue != nil && commandQueue.Len() > getMaxQueueLength() {
+func push(request *redigoRequest, queue *list.List) {
+	if queue != nil && queue.Len() > getMaxQueueLength() {
 		return
 	}
-	commandQueue.PushBack(request)
+	queue.PushBack(request)
 }
 
-func pop() *redigoRequest {
-	front := commandQueue.Front()
-	commandQueue.Remove(front)
+func pop(queue *list.List) *redigoRequest {
+	front := queue.Front()
+	queue.Remove(front)
 	p, ok := front.Value.(*redigoRequest)
 	if ok {
 		return p
